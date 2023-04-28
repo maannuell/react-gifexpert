d026902595cdcadbd671ddac574806e427d5f0b5
Manuel Meza <maannuell@gmail.com>
2023-04-28 12:44:22 -0700

    primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,11 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=KRJbkay5vitBKrm6HHjs2Hrleuf6M6VE&q=${category}&limit=10`;
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    return  data.map(img =>({
+        id: img.id,
+        title : img.title,
+        url: img.images.downsized_medium.url
+    }))
+}
